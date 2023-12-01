a974ee98c1e3e58eb66096664d31f0ace8303ad5
Nathan <127421398+nathan-cool@users.noreply.github.com>
2023-12-01 14:01:57 +0000

    added event lsitenr and the random math function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-function runGame() {
+
+document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.getElementsByTagName('button');
     
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            if (this.getAttribute('data-type') === 'submit') {
+                alert(" clicked the submit button");
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`You clicked the ${gameType} button`);
+            }
+        });
+    }
+});
+
+
+
+function runGame() {
+    let num1 = math.floor(math.random() * 25) + 1;
+    let num2 = math.floor(math.random() * 25) + 1;
+
 }
 
 function checkAnswer() {
